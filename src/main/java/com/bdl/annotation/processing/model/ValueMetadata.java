69a95a69796aed6aafd4addf4490aa2fb1a52347
Ben Leitner <bdleitner80@gmail.com>
2016-10-01 10:36:58 -0700

    Make ValueMetadata more public.

--- src/main/java/com/bdl/annotation/processing/model/ValueMetadata.java
+++ src/main/java/com/bdl/annotation/processing/model/ValueMetadata.java
@@ -13,7 +13,13 @@ public abstract class ValueMetadata {
   public abstract TypeMetadata type();
   public abstract String value();
 
-  static ValueMetadata create(TypeMetadata type, String value) {
+  public static ValueMetadata create(TypeMetadata type, String value) {
     return new AutoValue_ValueMetadata(type, value);
+  }
+
+  public static ValueMetadata create(Object object) {
+    return new AutoValue_ValueMetadata(
+        TypeMetadata.fromObject(object),
+        object.toString());
   }
 }
