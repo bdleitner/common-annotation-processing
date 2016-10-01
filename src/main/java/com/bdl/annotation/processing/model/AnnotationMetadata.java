823709539552af738366a8744397784b9254f568
Ben Leitner <bdleitner80@gmail.com>
2016-10-01 10:35:15 -0700

    Add a reference method to AnnotationMetadata to support Imports-aware toString, and make the class more public.

--- src/main/java/com/bdl/annotation/processing/model/AnnotationMetadata.java
+++ src/main/java/com/bdl/annotation/processing/model/AnnotationMetadata.java
@@ -6,6 +6,7 @@ import com.google.common.collect.ImmutableSet;
 
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.annotation.Nullable;
 import javax.lang.model.element.AnnotationMirror;
@@ -40,6 +41,26 @@ public abstract class AnnotationMetadata implements UsesTypes {
     return imports.build();
   }
 
+  public String reference(Imports imports) {
+    StringBuilder s = new StringBuilder("@");
+    s.append(type().reference(imports));
+    if (values().isEmpty()) {
+      return s.toString();
+    }
+    s.append("(");
+    if (values().size() == 1
+        && values().get("value") != null) {
+      // a single value with name "value" so we don't need the method name.
+      s.append("\"").append(values().get("value").value()).append("\"");
+    } else {
+      s.append(values().entrySet().stream()
+          .map(entry -> String.format("%s = \"%s\"", entry.getKey(), entry.getValue()))
+          .collect(Collectors.joining(", ")));
+    }
+    s.append(")");
+    return s.toString();
+  }
+
   public static AnnotationMetadata fromType(AnnotationMirror mirror) {
     Builder metadata = AnnotationMetadata.builder();
     metadata.setType(TypeMetadata.fromType(mirror.getAnnotationType()));
@@ -53,20 +74,20 @@ public abstract class AnnotationMetadata implements UsesTypes {
     return metadata.build();
   }
 
-  static Builder builder() {
+  public static Builder builder() {
     return new AutoValue_AnnotationMetadata.Builder();
   }
 
   @AutoValue.Builder
-  abstract static class Builder {
-    abstract Builder setType(TypeMetadata type);
+  public abstract static class Builder {
+    public abstract Builder setType(TypeMetadata type);
     abstract ImmutableMap.Builder<String, ValueMetadata> valuesBuilder();
 
-    Builder putValue(String methodName, ValueMetadata object) {
+    public Builder putValue(String methodName, ValueMetadata object) {
       valuesBuilder().put(methodName, object);
       return this;
     }
 
-    abstract AnnotationMetadata build();
+    public abstract AnnotationMetadata build();
   }
 }
